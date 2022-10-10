d884872bbc50db480827a9410e762124754b6128
Chloe Dorogoyer <chloed@nycstudents.net>
2022-10-10 12:04:37 -0400

    new print statements

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(int a) {
+        x = a;
+        y = a;
+    }
+
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int newX) {
+        x = newX;
+    }
+
+    public void setY(int newY) {
+        y = newY;
+    }
+
+    public String coordinate() {
+        return "(" + x + "," + y + ")";
+    }
+
+    public String quadrant() {
+        String quadrant;
+        if (x > 0 && y > 0) {
+            quadrant = "I";
+        } else if (x < 0 && y > 0) {
+            quadrant = "II";
+        } else if (x < 0 && y < 0) {
+            quadrant = "III";
+        } else if (x > 0 && y < 0) {
+            quadrant = "IV";
+        } else {
+            quadrant = "on an axis";
+        }
+        return quadrant;
+
+    }
 }
